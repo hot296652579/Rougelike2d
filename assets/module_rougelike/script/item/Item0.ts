e500aeb07796fd72230baaaf4ad830ac8a7ce886
super_javan <296652579@qq.com>
2024-07-26 12:53:58 +0800

    禁止刚体

--- assets/module_rougelike/script/item/Item0.ts
+++ assets/module_rougelike/script/item/Item0.ts
@@ -2,7 +2,7 @@
  * @Author: super_javan 296652579@qq.com
  * @Date: 2024-07-19 16:01:53
  * @LastEditors: super_javan 296652579@qq.com
- * @LastEditTime: 2024-07-19 17:26:23
+ * @LastEditTime: 2024-07-19 22:40:06
  * @FilePath: /RougelikeGame2D/assets/module_rougelike/script/item/Item0.ts
  * @Description: 物品1,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,6 +15,7 @@ const { ccclass, property } = _decorator;
 @ccclass('Item0')
 export class Item0 extends Item {
     private shouldDestroy: boolean = false;
+
     start() {
         this.initialize({ name: "Weapon1", attributeBonuses: [{ type: AttributeType.Attack, value: 10 }] });
 
@@ -36,13 +37,13 @@ export class Item0 extends Item {
 
     onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
         // console.log('物品碰撞检测 other group:', otherCollider.group);
-        const otherGroupName = groupNames[otherCollider.group] || 'Unknown'
+        const otherGroupName = groupNames[otherCollider.group] || 'Unknown';
+
         if (otherGroupName == groupNames[2]) {
             const player = otherCollider.getComponent(PlayerController);
             if (player) {
+                contact.disabledOnce = true;
                 player.equipItem(this);
-                this.rigidBody2D.enabled = false;
-                this.collider2D.enabled = false;
                 this.shouldDestroy = true;
             }
         }
